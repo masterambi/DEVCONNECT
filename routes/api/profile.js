ffd6f8769411bcdcd58a537f3c16e6e675eef6d2
masterambi <ramzyrash@gmail.com>
2020-04-09 13:41:20 +0700

    Create profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/', auth, async (req, res) => {
+
+  try {
+    
+  } catch (err) {
+    console.error(err.message);
+  }
+
+});
 
 module.exports = router;
