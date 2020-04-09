6fb349fa219f428ca2200d68b5bb767ccdf513f2
masterambi <ramzyrash@gmail.com>
2020-04-09 16:19:02 +0700

    Add get all profiles and profile by user ID

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: true,
+      useFindAndModify: false,
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected...');
