183d59dfec63d0d20d0f3c4c601b660b9aa1aa27
masterambi <ramzyrash@gmail.com>
2020-04-09 17:39:13 +0700

    Add add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,7 +27,7 @@ router.get('/me', auth, async (req, res) => {
 });
 
 // @route    POST api/profile
-// @desc     Create of update user profile
+// @desc     Create and update user profile
 // @access   Private
 router.post(
   '/',
@@ -178,7 +178,43 @@ router.put(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    }
+
+
   }
 );
+
 
 module.exports = router;
