d046b33c1bd5a1a9ef5146fd7a6d7471ec10d985
masterambi <ramzyrash@gmail.com>
2020-04-09 18:47:04 +0700

    Add get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -328,7 +328,19 @@ router.get('/github/:username', (req, res) => {
       }/repos?per_page=5&sort=created:asc&client_id=${config.get(
         'githubClientId'
       )}&client_secret=${config.get('githubSecret')}}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
     };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
