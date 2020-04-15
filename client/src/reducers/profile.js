7e166656c5f005dc3096478cd766d203c1b93dd8
masterambi <ramzyrash@gmail.com>
2020-04-15 18:31:56 +0700

    Set up dashboard

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const intitialState = {
   profile: null,
@@ -24,6 +24,13 @@ export default function (state = intitialState, action) {
         error: payload,
         loading: false,
       };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      }
     default:
       return state;
   }
